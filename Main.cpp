20e190f5fd45767555e9967a60c1469dbac6ce35
smeridian387 <43002188+smeridian387@users.noreply.github.com>
2018-09-12 16:12:44 +0100

    created base window added button masher assets

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,77 @@
+//included library section
 #include <SFML/Graphics.hpp>
+#include <SFML\Audio.hpp>
+#include <string>
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	//game setup-------------------------------------------------------------------
 
-	while (window.isOpen())
+	// make a variable called a gamewindow of type RenderWindow
+	sf::RenderWindow gameWindow;
+	gameWindow.create(sf::VideoMode::getDesktopMode(), "Quick draw",
+		sf::Style::Titlebar | sf::Style::Close);
+
+	//timer functionality
+	float signalTimeLowerLimit = 5.0f;
+	float signalTimeUpperLimit = 10.0f;
+	sf::Time timeTillSignal = sf::seconds(0.0f);
+	sf::Time timeSinceSignal = sf::seconds(0.0f);
+	sf::Clock gameClock;
+
+	//create button sprite
+	sf::Texture buttonTexture;
+	buttonTexture.loadFromFile("graphics/button.png");
+
+	sf::Sprite buttonSprite;
+	buttonSprite.setTexture(buttonTexture);
+	buttonSprite.setPosition(
+	gameWindow.getSize().x / 2 - buttonTexture.getSize().x / 2,
+	gameWindow.getSize().y / 2 - buttonTexture.getSize().y / 2);
+
+
+	//end game set up--------------------------------------------------------------
+
+
+
+
+	//game loop-------------------------------------------------------------------
+	//runs every frame untill the game window is closed
+	while (gameWindow.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+		//------------------------------------------------------------------------
+		//input
+		//------------------------------------------------------------------------
+		// Check for input
+		sf::Event gameEvent;
 
+		while (gameWindow.pollEvent(gameEvent))
+		{
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				//close game window
+				gameWindow.close();
+
+			}//END CLOSE EVENT
+
+		}//END POLLING WHILE LOOP
+		//------------------------------------------------------------------------
+		//end input
+		//------------------------------------------------------------------------
+
+		//------------------------------------------------------------------------
+		//update
+		//------------------------------------------------------------------------
+		//update the clock each frame
+		sf::Time frameTime = gameClock.restart();
+		//------------------------------------------------------------------------
+		//end update
+		//------------------------------------------------------------------------
+
+		//Clear the window to a single color
+		gameWindow.clear(sf::Color::Black);
+
+		//Draw section
+		gameWindow.draw(buttonSprite);
+	}//END WINDOW OPEN WHILE LOOP
 	return 0;
-}
+}// END MAIN FUNCTION
