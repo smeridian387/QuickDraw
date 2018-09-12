d6f8baa17a83746e59e83bdead7e83b45e00aafc
smeridian387 <43002188+smeridian387@users.noreply.github.com>
2018-09-12 16:47:57 +0100

    added, mouse input, button colour changing

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML\Audio.hpp>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 int main()
 {
 	//game setup-------------------------------------------------------------------
@@ -11,17 +13,19 @@ int main()
 	gameWindow.create(sf::VideoMode::getDesktopMode(), "Quick draw",
 		sf::Style::Titlebar | sf::Style::Close);
 
+	//seed our random number generator
+	srand(time(NULL));
+
 	//timer functionality
 	float signalTimeLowerLimit = 5.0f;
 	float signalTimeUpperLimit = 10.0f;
-	sf::Time timeTillSignal = sf::seconds(0.0f);
+	sf::Time timeTillSignal = sf::seconds(5.0f);
 	sf::Time timeSinceSignal = sf::seconds(0.0f);
 	sf::Clock gameClock;
 
 	//create button sprite
 	sf::Texture buttonTexture;
 	buttonTexture.loadFromFile("graphics/button.png");
-
 	sf::Sprite buttonSprite;
 	buttonSprite.setTexture(buttonTexture);
 	buttonSprite.setPosition(
@@ -57,12 +61,38 @@ int main()
 		//------------------------------------------------------------------------
 		//end input
 		//------------------------------------------------------------------------
+		//TOD:process events
+		if (gameEvent.type == sf::Event::MouseButtonPressed)
+		{
+			//check if we hit the button
+			if (buttonSprite.getGlobalBounds().contains(gameEvent.mouseButton.x, gameEvent.mouseButton.y))
+			{
+				//we did!
+				int range = (int)signalTimeUpperLimit - signalTimeLowerLimit;
+				float signalSeconds = rand() % range + signalTimeLowerLimit;
+				timeTillSignal = sf::seconds(signalSeconds);
+			}
+		}
+		// check if the event is the close event
+		if (gameEvent.type == sf::Event::Closed)
+		{
+			//close the game window
+			gameWindow.close();
+		}
+	
 
 		//------------------------------------------------------------------------
 		//update
 		//------------------------------------------------------------------------
 		//update the clock each frame
 		sf::Time frameTime = gameClock.restart();
+		timeTillSignal = timeTillSignal - frameTime;
+
+		if (timeTillSignal.asSeconds() <= 0.0f)
+		{
+			buttonSprite.setColor(sf::Color::Red);
+
+		}
 		//------------------------------------------------------------------------
 		//end update
 		//------------------------------------------------------------------------
@@ -72,6 +102,8 @@ int main()
 
 		//Draw section
 		gameWindow.draw(buttonSprite);
+
+		gameWindow.display();
 	}//END WINDOW OPEN WHILE LOOP
 	return 0;
 }// END MAIN FUNCTION
